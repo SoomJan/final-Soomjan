658e2415a4fa8e65a5c78c24213d4009b0c6bd09
soonpyo21 <tnsvy2004@gmail.com>
2021-11-30 17:02:28 +0900

    11/30 17:02 권순표

---
+++ src/main/java/com/soomjd/soomjan/member/controller/MemberController.java
@@ -0,0 +1,35 @@
+package com.soomjd.soomjan.member.controller;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.soomjd.soomjan.member.model.service.MemberService;
+
+//@Controller
+//@RequestMapping("/*")
+public class MemberController {
+
+//	private MemberService memberService;
+//
+//	@Autowired
+//	public MemberController(MemberService memberService) {
+//		this.memberService = memberService;
+//	}
+//
+//
+//	@RequestMapping(value = {"/", "main"})
+//	public String main() {
+//
+//		List<HashMap<String, Object>> memberList = memberService.selectTest();
+//		for(HashMap<String, Object> member : memberList) {
+//			System.out.println(member);
+//		}
+//
+//		return "main/main";
+//	}
+
+}
