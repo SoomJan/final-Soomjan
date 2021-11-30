3f80cba06ce880364d1d672c842b6add99fac8b3
Lim-yeram0629 <ysyr0830@gmail.com>
2021-11-30 17:02:39 +0900

    zzzzz

---
+++ src/main/java/com/soomjd/soomjan/main/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.soomjd.soomjan.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/*")
+public class MainController {
+	
+	@RequestMapping(value = {"/", "main"})
+	public String main() {
+		
+		return "mentor/class/classChat";
+	}
+	
+}
