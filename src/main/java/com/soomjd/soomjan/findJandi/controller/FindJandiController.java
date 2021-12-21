a8cb993481ffdbeac25489e7a331ab8fa2c9d207
Soobeen2 <osb0903@hotmail.com>
2021-12-21 17:36:45 +0900

    2021-12-21 오수빈(화1) findJandi 서비스,dto,dao,mapper....

--- src/main/java/com/soomjd/soomjan/findJandi/controller/FindJandiController.java
+++ src/main/java/com/soomjd/soomjan/findJandi/controller/FindJandiController.java
@@ -1,15 +1,50 @@
 package com.soomjd.soomjan.findJandi.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.soomjd.soomjan.findJandi.model.service.FindJandiService;
+import com.soomjd.soomjan.jandi.model.dto.JandiDTO;
 
 @Controller
 @RequestMapping("/findJandi/*")
 public class FindJandiController {
-
+	/*
+	 * controller의 역할
+	 * 1. JSP페이지에서 전달한 값의 정제
+	 * 2. 서비스를 요청
+	 * 3. 값의 결과에 따라서 보여줄 페이지를 정의
+	 * */
+	
+	// controller에서 Service를 요청하려면 
+	// controller에서는 service에 대한 정보를 가지고 있어야한다.
+	// has-a 관계이다.
+	
+	// findJandiService를 요청하기 위한 변수
+	private final FindJandiService findJandiService;
+	
+	// 매개변수
+	// 매개변수 있는 생성자를 이용해서 의존성 주입
+	@Autowired
+	public FindJandiController(FindJandiService findJandiService) {
+		this.findJandiService = findJandiService;
+	}
+	
+	// 멘토보기를 클릭했을 때 호출되는 메소드
 	@GetMapping("/findAllJandiMain")
-	public void findAllJandiMain() {}
+	public void findAllJandiMain(Model model) {
+		
+		// 잔디 목록 조회하기
+		List<JandiDTO> jandiList = findJandiService.selectfindJandi();
+		System.out.println("jandiList : " + jandiList);
+		
+		model.addAttribute("jandiList", jandiList);
+	}
 	
 	@GetMapping("/findTopJandiMain")
 	public void findTopJandiMain() {}
