6b9ab9965149ba08bc04e2af3d15242efe5ac665
Soobeen2 <osb0903@hotmail.com>
2021-12-24 18:06:25 +0900

    2021-12-24 오수빈(금) 전체잔디 페이징

--- src/main/java/com/soomjd/soomjan/findJandi/model/dao/FindJandiMapper.java
+++ src/main/java/com/soomjd/soomjan/findJandi/model/dao/FindJandiMapper.java
@@ -2,9 +2,12 @@ package com.soomjd.soomjan.findJandi.model.dao;
 
 import java.util.List;
 
+import com.soomjd.soomjan.common.paging.SelectCriteria;
 import com.soomjd.soomjan.jandi.model.dto.JandiDTO;
 
 public interface FindJandiMapper {
 
-	List<JandiDTO> selectFindJandi();
+	List<JandiDTO> selectFindJandi(SelectCriteria selectCriteria);
+
+	int SelectFindJandiTotalCount();
 }
--- src/main/java/com/soomjd/soomjan/findJandi/model/service/FindJandiService.java
+++ src/main/java/com/soomjd/soomjan/findJandi/model/service/FindJandiService.java
@@ -1,14 +1,18 @@
 package com.soomjd.soomjan.findJandi.model.service;
 
 import java.util.List;
+import java.util.Map;
 
+import com.soomjd.soomjan.common.paging.SelectCriteria;
 import com.soomjd.soomjan.jandi.model.dto.JandiDTO;
 
 public interface FindJandiService {
 
-	List<JandiDTO> selectfindJandi();
+	List<JandiDTO> selectfindJandi(SelectCriteria selectCriteria);
 	
 	List<JandiDTO> mainJandi();
 
 	List<JandiDTO> selectTopFindJandiList();
+
+	int SelectFindJandiTotalCount(Map<String, Object> searchMap);
 }
